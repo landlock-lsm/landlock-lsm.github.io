d09987c0edd5e158fed7faf9194459e427ec05f8
Mickaël Salaün <mic@digikod.net>
2023-02-05 18:24:31 +0100

    make-and-rsync: Create a commit in website/

--- make-and-rsync.sh
+++ make-and-rsync.sh
@@ -5,3 +5,14 @@ set -u -e -o pipefail
 make html
 rm -r website/_static/
 rsync --archive --human-readable --human-readable --info=progress2 _build/html/ website/
+
+if [[ -n "$(git status --short --porcelain)" ]]; then
+	echo "ERROR: Dirty worktree" >&2
+	exit 1
+fi
+COMMIT="$(git log --no-walk --format=%H)"
+
+pushd website
+git add -A
+git commit --edit -m "index: XXX" -m "Built from commit ${COMMIT}"
+popd
