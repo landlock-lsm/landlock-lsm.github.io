9a607d9a84ce7d91dcd5964481de017f025f18d8
Mickaël Salaün <mic@digikod.net>
2025-07-16 20:54:45 +0200

    github: Check, build, and deploy website
    Remove now-useless files: CNAME and .nojekyll
    https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- make-and-rsync.sh
+++ make-and-rsync.sh
@@ -30,14 +30,3 @@ rsync \
 	--info=progress2 \
 	_build/html/* talks \
 	website/
-
-if [[ -n "$(git status --short --porcelain)" ]]; then
-	echo "ERROR: Dirty worktree" >&2
-	exit 1
-fi
-COMMIT="$(git log --no-walk --format=%H)"
-
-pushd website
-git add -A
-git commit --edit -m "index: XXX" -m "Built from commit ${COMMIT}"
-popd
